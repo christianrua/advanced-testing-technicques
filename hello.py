ddfa687833b048fd9e1ea426e219e479bac5d246
Christian Rua <christian.rua@gmail.com>
2021-07-31 09:44:12 -0500

    adding the hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,14 @@
 def addThis(x,y):
     print(f"This is x: {x} and the type is {type(x)}, and this is y: {y}")
     print(f"This is y: {y} and the y-type is {type(y)}")
-    result = x + y
+
+    try:
+        result = x + y
+    except TypeError as exception:
+        print(f"The wrong types passed in {exception}")
+        result = int(x) + int(y)
+
     print(f"This is the result: {result}")
     return x+y
 
-print(addThis(1,2))    
+print(addThis("1",2))    
